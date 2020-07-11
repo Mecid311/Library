22425732fdf547eb261a21f396ec42584c0b6d87
Majid Nacafov <53233065+Mecid311@users.noreply.github.com>
2020-07-11 20:25:39 +0400

    Add files via upload

---
+++ Code-Library/BookLibrarySolution/LibraryConsoleApp/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryConsoleApp
+{
+    class Book
+    {
+        public string name;
+        public string authorName;
+        public int publishedDate;
+        int pageCount;
+
+        public int PageCount {
+            get
+            {
+                return pageCount;
+            }
+            set
+            {
+                if (value<10)
+                {
+                    throw new Exception("pageCount standarta uygun deyil.!");
+                }
+                pageCount = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name : {name} ," +
+                $"AuthorName : {authorName} ," +
+                $"PublishedDate : {publishedDate}\n";
+               
+        }
+        public string NewListLabrary()
+        {
+            return $"Name       : {name}\n" +
+                $"AuthorName    : {authorName}\n" +
+                $"PublishedDate : {publishedDate}\n" +
+                $"PageCount     :{pageCount}\n";
+        }
+
+    }
+}
---
+++ Code-Library/BookLibrarySolution/LibraryConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book Library = new Book();
+            Library.name = "Qerb burkusu";
+            Library.authorName = "Çingiz Abdullayev";
+            Library.publishedDate = 2013;
+            Library.PageCount = 13;
+
+            Console.WriteLine(Library);
+            
+            List<Book> NewLibrary = new List<Book>();
+            NewLibrary.Add(new Book()
+            {
+                name = "Etiraflar vadisi",
+                authorName = "Çingiz Abdullaye",
+                publishedDate = 2009,
+                PageCount = 250
+            });
+
+            NewLibrary.Add(new Book()
+              {
+                  name = "George Orwell",
+                  authorName = "Corc Oruell",
+                  publishedDate = 2009,
+                  PageCount = 174
+              });
+
+            NewLibrary.Add(new Book()
+            {
+                name = "Lev Tolstoy",
+                authorName = "Anna Karenina",
+                publishedDate = 1878,
+                PageCount = 124
+            });
+
+            NewLibrary.Add(new Book()
+            {
+               name = "Lev Tolstoy",
+               authorName = "Anna Karenina",
+               publishedDate = 1878,
+               PageCount = 124,
+        });
+
+            for (int i = 0; i < NewLibrary.Count; i++)
+            {
+                Console.WriteLine(NewLibrary[i].NewListLabrary());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
